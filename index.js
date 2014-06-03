a64172ca162364b63e038bd8ac31b562b2b8aeb2
Gregory <christopher.gregory@GS022955.local>
2014-06-03 13:54:49 -0700

    Added feature to draw noise over CAPTCHA image with configurable noise level

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var Captcha = require('./lib/Captcha');
 var drawBackground = require('./lib/middle/drawBackground');
 var drawText = require('./lib/middle/drawText');
 var drawLines = require('./lib/middle/drawLines');
+var drawNoise = require('./lib/middle/drawNoise');
 
 var getColors = require('./lib/getColors');
 var getFontSize = require('./lib/getFontSize');
@@ -14,6 +15,7 @@ module.exports = {
   drawBackground: drawBackground,
   drawText: drawText,
   drawLines: drawLines,
+  drawNoise: drawNoise,
 
   // utils
   getColors: getColors,
@@ -26,6 +28,7 @@ module.exports = {
     cap.use(drawBackground);
     cap.use(drawLines);
     cap.use(drawText);
+    cap.use(drawNoise);
     cap.use(drawLines);
     return cap;
   }
