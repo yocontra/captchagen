4b69a74dd9122a2610bf36da773d209bea9f8eaa
邹慧东 <huidong.zou@niu.com>
2016-03-06 00:41:23 +0800

    drawImage optional

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var Captcha = require('./lib/Captcha');
 var drawBackground = require('./lib/middle/drawBackground');
 var drawText = require('./lib/middle/drawText');
 var drawLines = require('./lib/middle/drawLines');
+var drawPoints = require('./lib/middle/drawPoints');
+var drawImage = require('./lib/middle/drawImage');
 
 var getColors = require('./lib/getColors');
 var getFontSize = require('./lib/getFontSize');
@@ -14,6 +16,8 @@ module.exports = {
   drawBackground: drawBackground,
   drawText: drawText,
   drawLines: drawLines,
+  drawPoints: drawPoints,
+  drawImage: drawImage,
 
   // utils
   getColors: getColors,
@@ -21,12 +25,40 @@ module.exports = {
   generateText: generateText,
   
   // use default settings
-  create: function (opt) {
+  create: function (opt,drawArguments) {
     var cap = new Captcha(opt);
-    cap.use(drawBackground);
-    cap.use(drawLines);
+    var obj = drawArguments || {};
+
+
+    if (isEmptyObj(obj)) {
+      cap.use(drawBackground);
+      if (opt.img) {
+       cap.use(drawImage);
+      }
+      cap.use(drawText);
+      cap.use(drawLines);
+      cap.use(drawPoints);
+      return cap;
+    }
+    if (obj.drawBackground) {
+      cap.use(drawBackground);
+    }
+    if (obj.drawImage && opt.img) {
+      cap.use(drawImage);
+    }
+    if (obj.drawLines) {
+      cap.use(drawLines);
+    }
+    if (obj.drawPoints) {
+      cap.use(drawPoints);
+    }
     cap.use(drawText);
-    cap.use(drawLines);
     return cap;
   }
 };
+function isEmptyObj (obj){
+  for(var key in obj){
+    return false;
+  }
+  return true;
+}
